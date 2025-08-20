a7fab2061fd43a850b961dc7c27010bd8376ca7f
shosha-io <20241700031@cis.asu.edu.eg>
2025-08-21 00:13:48 +0200

    Delete FOOTBALL directory

--- FOOTBALL/src/soccer/event/KICKOFF.java
+++
@@ -1,9 +0,0 @@
-package soccer.event;
-
-public class KICKOFF extends GameEvent {
-    @Override
-    public String gameevent() {
-        return "Kickoff by " + getTeam().getTeamName() +
-                " at " + getStartTime() + " minutes.";
-    }
-}
